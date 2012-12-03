5cb206e7f41b28ce2d3885fd7393f1c91e0f56df
Thomas Bischof <tbischofd@mit.edu>
2012-12-03 12:39:13 -0500

    Initial commit of an outline of things to come.

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name="pypicoquant",
+      version="0.0.1",
+      author="Thomas Bischof",
+      author_email="tbischof2mit.edu",
+      description="A ctypes interface to Picoquant hardware libraries and types.",
+      packages=["picoquant"])
