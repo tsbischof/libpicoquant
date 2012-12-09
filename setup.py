dc2fa9c47d667812bee90cd53aca13ce3ef949f6
Thomas Bischof <tbischofd@mit.edu>
2012-12-09 13:54:38 -0500

    Fixed building on Linux. In the future, add conditional checks to ensure that the library directories exist. Also make sure that python versions make it into the linux dist.

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ from distutils.core import setup, Extension
 
 is_64bits = sys.maxsize > 2**31
 
-lib_dir = "/usr/local/lib"
+unix_lib_dir = os.path.join("/", "usr", "local", "lib")
 if is_64bits:
-    lib_dir += "64"
+    unix_lib_dir += "64"
 
 ext_modules = list()
 # PicoHarp only has 32-bit library, and must be built with 32-bit Python.
@@ -17,7 +17,7 @@ if sys.platform ==  "win32":
                                  "PH300-PHLibv23")]
     picoharp_libs = ["phlib"]
 else:
-    picoharp_dirs = [os.path.join(lib_dir, "ph300")]
+    picoharp_dirs = [os.path.join(unix_lib_dir, "ph300")]
     picoharp_libs = ["ph300"]
     
 picoharp_module = Extension(
@@ -26,7 +26,7 @@ picoharp_module = Extension(
       os.path.join("picoquant", "comm_lib_common.i")],
      library_dirs=picoharp_dirs,
      include_dirs=picoharp_dirs,
-     swig_opts=["-I {0}".format(my_dir) for my_dir in picoharp_dirs],
+     swig_opts=["-I{0}".format(my_dir) for my_dir in picoharp_dirs],
      libraries=picoharp_libs)
 
 if is_64bits:
@@ -42,7 +42,7 @@ if sys.platform == "win32":
                                    "HydraHarp-HHLibv20")]
     hydraharp_libs = ["hhlib"]
 else:
-    hydraharp_dirs = [os.path.join(lib_dir, "hh400")]
+    hydraharp_dirs = [os.path.join(unix_lib_dir, "hh400")]
     hydraharp_libs = ["hh400"]
     
 hydraharp_module = Extension(
@@ -51,7 +51,7 @@ hydraharp_module = Extension(
       os.path.join("picoquant", "comm_lib_common.i")],
      library_dirs=hydraharp_dirs,
      include_dirs=hydraharp_dirs,
-     swig_opts=["-I {0}".format(my_dir) for my_dir in hydraharp_dirs],
+     swig_opts=["-I{0}".format(my_dir) for my_dir in hydraharp_dirs],
      libraries=hydraharp_libs)
 # HydraHarp has 32-bit and 64-bit libraries
 ext_modules.append(hydraharp_module)
